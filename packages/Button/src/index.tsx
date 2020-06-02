24b794cddd986de06c7a3f757e34b2f46830160a
PengfeiYuan <yuanpengfei@itiger.com>
2020-06-02 20:05:41 +0800

    disable prop

--- packages/Button/src/index.tsx
+++ packages/Button/src/index.tsx
@@ -4,17 +4,21 @@ import "bulma/css/bulma.css";
 interface IButtonProps {
   onClick?(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void;
   type?: "danger" | "primary" | "link" | "info" | "success" | "warning";
+  disabled?: boolean;
 }
 
 const Button: React.FC<IButtonProps> = (props) => {
   const className = props.type ? `button is-${props.type}` : "button";
 
   const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (props.disabled) {
+      return;
+    }
     props.onClick && props.onClick(e);
   };
 
   return (
-    <button className={className} onClick={onClick}>
+    <button className={className} onClick={onClick} disabled={props.disabled}>
       {props.children || "Button"}
     </button>
   );
@@ -22,6 +26,7 @@ const Button: React.FC<IButtonProps> = (props) => {
 
 Button.defaultProps = {
   onClick: () => alert("I am Button"),
+  disabled: false,
 };
 
 export default Button;
