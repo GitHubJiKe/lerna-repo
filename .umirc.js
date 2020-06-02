1378ab874b0756e7634a2d68ac3620bf5b3ed2a1
PengfeiYuan <yuanpengfei@itiger.com>
2020-06-02 19:15:28 +0800

    update umirc

--- .umirc.js
+++ .umirc.js
@@ -6,8 +6,7 @@ export default {
   favicon: "https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg",
   navs: [
     null,
-    { title: "GitHub", path: "https://github.com/umijs/dumi" },
-    { title: "更新日志", path: "https://github.com/umijs/dumi/releases" },
+    { title: "GitHub", path: "https://github.com/GitHubJiKe/lerna-repo" },
   ],
   extraBabelPlugins: [
     [
@@ -19,7 +18,5 @@ export default {
       },
     ],
   ],
-  scripts: ["https://v1.cnzz.com/z_stat.php?id=1278653578&web_id=1278653578"],
-  styles: ["a[title=站长统计] { display: none; }"],
   exportStatic: {},
 };
